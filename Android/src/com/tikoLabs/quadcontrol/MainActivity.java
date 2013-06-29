1d8fcb14e0f93d8fda95754c84e47453008afc4c
billhsu <billhsu.x@gmail.com>
2013-06-29 21:52:39 +0800

    change layout

--- Android/src/com/tikoLabs/quadcontrol/MainActivity.java
+++ Android/src/com/tikoLabs/quadcontrol/MainActivity.java
@@ -67,11 +67,11 @@ public class MainActivity extends Activity {
     }
 
 
-    @Override
+    /*@Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
-    }
+    }*/
     
 }
