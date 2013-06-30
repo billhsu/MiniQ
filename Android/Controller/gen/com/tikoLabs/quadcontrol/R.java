c187b3556ce5abe886290add596677b051b739f5
billhsu <billhsu.x@gmail.com>
2013-06-30 15:50:43 +0800

    Add bluetooth service.

--- Android/Controller/gen/com/tikoLabs/quadcontrol/R.java
+++ Android/Controller/gen/com/tikoLabs/quadcontrol/R.java
@@ -24,14 +24,21 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080004;
+        public static final int action_settings=0x7f080009;
         public static final int angleTextView=0x7f080001;
         public static final int btnConnect=0x7f080003;
+        public static final int button_scan=0x7f080008;
         public static final int joystickView=0x7f080000;
+        public static final int new_devices=0x7f080007;
+        public static final int paired_devices=0x7f080005;
         public static final int powerTextView=0x7f080002;
+        public static final int title_new_devices=0x7f080006;
+        public static final int title_paired_devices=0x7f080004;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
+        public static final int device_list=0x7f030001;
+        public static final int device_name=0x7f030002;
     }
     public static final class menu {
         public static final int main=0x7f070000;
@@ -39,7 +46,28 @@ public final class R {
     public static final class string {
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
+        public static final int bt_not_enabled_leaving=0x7f050005;
+        public static final int button_scan=0x7f05000f;
+        /**  Options Menu 
+         */
+        public static final int connect=0x7f050010;
+        public static final int discoverable=0x7f050011;
         public static final int hello_world=0x7f050002;
+        public static final int none_found=0x7f05000c;
+        public static final int none_paired=0x7f05000b;
+        public static final int not_connected=0x7f050004;
+        /**   DeviceListActivity 
+         */
+        public static final int scanning=0x7f050009;
+        public static final int select_device=0x7f05000a;
+        /**   Bluetooth 
+         */
+        public static final int send=0x7f050003;
+        public static final int title_connected_to=0x7f050007;
+        public static final int title_connecting=0x7f050006;
+        public static final int title_not_connected=0x7f050008;
+        public static final int title_other_devices=0x7f05000e;
+        public static final int title_paired_devices=0x7f05000d;
     }
     public static final class style {
         /** 
--- Android/Controller/src/com/tikoLabs/quadcontrol/MainActivity.java
+++ Android/Controller/src/com/tikoLabs/quadcontrol/MainActivity.java
@@ -9,6 +9,24 @@ import android.view.Menu;
 import android.widget.TextView;
 
 public class MainActivity extends Activity {
+	// Debugging
+    private static final String TAG = "BluetoothChat";
+    private static final boolean D = true;
+
+    // Message types sent from the BluetoothChatService Handler
+    public static final int MESSAGE_STATE_CHANGE = 1;
+    public static final int MESSAGE_READ = 2;
+    public static final int MESSAGE_WRITE = 3;
+    public static final int MESSAGE_DEVICE_NAME = 4;
+    public static final int MESSAGE_TOAST = 5;
+
+    // Key names received from the BluetoothChatService Handler
+    public static final String DEVICE_NAME = "device_name";
+    public static final String TOAST = "toast";
+
+    // Intent request codes
+    private static final int REQUEST_CONNECT_DEVICE = 1;
+    private static final int REQUEST_ENABLE_BT = 2;
     private TextView angleTextView;
     private TextView powerTextView;
     private TextView directionTextView;
